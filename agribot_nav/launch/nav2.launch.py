6071d9b36fe245b81d2f8f64e241461a9b3ef428
SushantNiraula <sushantniraula01@gmail.com>
2026-02-17 07:50:41 +0545

    Working on nav2, lots of work left to be done.

---
+++ agribot_nav/launch/nav2.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+
+        Node(
+            package='nav2_bringup',
+            executable='bringup_launch.py',
+            output='screen',
+            parameters=[
+                {'use_sim_time': True},
+                '/path/to/nav2_params.yaml'
+            ]
+        )
+
+    ])
